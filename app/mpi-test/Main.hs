97f3dc95bd46228d325033619847a017ba430fd5
Gabriel Riera <gabriel.riera@uib.es>
2019-07-22 13:42:47 +0200

    wip

--- app/mpi-test/Main.hs
+++ app/mpi-test/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Data.List (genericLength)
 import Data.Text (Text)
 
+import Control.Monad.IO.Class (liftIO)
 import qualified Control.Distributed.MPI.Store as MPI
 
 import Pipes ((>->))
@@ -19,11 +20,14 @@ import qualified VPF.Util.MPI        as M
 data MyTag = MyTag
   deriving (Eq, Ord, Show, Bounded, Enum)
 
-inputTag :: M.JobInput MyTag [FA.FastaEntry]
-inputTag = M.JobInput MyTag
+inputTag :: M.JobTagIn MyTag [FA.FastaEntry]
+inputTag = M.JobTagIn MyTag
 
-resultTag :: M.JobResult MyTag [Text]
-resultTag = M.JobResult MyTag
+resultTag :: M.JobTagOut MyTag [Text]
+resultTag = M.JobTagOut MyTag
+
+jobTags :: M.JobTags MyTag MyTag [Int] Int
+jobTags = M.JobTags (M.JobTagIn MyTag) (M.JobTagOut MyTag)
 
 
 main :: IO ()
@@ -35,20 +39,23 @@ main = MPI.mainMPI $ do
   case MPI.fromRank rank of
     0 -> do
         r <- rootMain (succ rank `enumFromTo` pred size) comm
-        case r of
-          Right () -> return ()
-          Left e   -> print e
-    _ -> workerMain MPI.rootRank comm
+        print r
+    _ -> workerMain MPI.rootRank rank comm
 
 
-rootMain :: [MPI.Rank] -> MPI.Comm -> IO (Either FA.ParseError ())
+rootMain :: [MPI.Rank] -> MPI.Comm -> IO [Int] -- (Either FA.ParseError ())
 rootMain slaves comm = do
-    PS.runSafeT $ P.runEffect $
-        fastaProducer
-        >-> M.delegate slaves inputTag resultTag comm (genericLength slaves * 2)
-        >-> P.chain (P.liftIO . print)
-        >-> P.for P.cat (\as -> sequence_ [P.yield a | a <- as])
-        >-> FS.stdoutWriter
+    PS.runSafeT $ P.toListM $
+        Conc.bufferedChunks 5 (P.each [0..50])
+          >-> P.chain (\ns -> liftIO $ putStrLn $ "main: submitting " ++ show ns)
+          >-> M.delegate slaves jobTags comm (genericLength slaves)
+          >-> P.chain (\s -> liftIO $ putStrLn $ "main: output " ++ show s)
+
+--         fastaProducer
+--         >-> M.delegate slaves inputTag resultTag comm (genericLength slaves * 2)
+--         >-> P.chain (P.liftIO . print)
+--         >-> P.for P.cat (\as -> sequence_ [P.yield a | a <- as])
+--         >-> FS.stdoutWriter
   where
     fastaProducer :: P.Producer [FA.FastaEntry] (PS.SafeT IO) (Either FA.ParseError ())
     fastaProducer =
@@ -57,11 +64,10 @@ rootMain slaves comm = do
     fileLines = FS.fileReader "../data-backup/All_Viral_Contigs_4filters_final.fasta"
 
 
-workerMain :: MPI.Rank -> MPI.Comm -> IO ()
-workerMain master comm = do
-    myRank <- MPI.commRank comm
-
-    PS.runSafeT $ M.makeWorker master inputTag resultTag comm $ \chunk -> do
-        let ns = [n | FA.FastaEntry n _ <- chunk]
-        P.liftIO $ putStrLn $ show myRank ++ " processed " ++ show (length ns) ++ " lines"
-        return ns
+workerMain :: MPI.Rank -> MPI.Rank -> MPI.Comm -> IO ()
+workerMain master me comm = do
+    PS.runSafeT $ M.makeWorker master jobTags comm $ \ns -> do
+        -- let ns = [n | FA.FastaEntry n _ <- chunk]
+        let s = sum ns
+        liftIO $ putStrLn $ "worker" ++ show me ++ ": processing " ++ show ns ++ " = " ++ show s
+        return s
--- src/VPF/Util/MPI.hs
+++ src/VPF/Util/MPI.hs
@@ -1,5 +1,6 @@
 {-# language DeriveGeneric #-}
 {-# language GeneralizedNewtypeDeriving #-}
+{-# language StrictData #-}
 module VPF.Util.MPI where
 
 import GHC.Generics (Generic)
@@ -10,8 +11,10 @@ import qualified Control.Concurrent.Async       as Async
 import qualified Control.Concurrent.STM.TBQueue as TBQ
 import qualified Control.Concurrent.STM.TMVar   as TMVar
 import qualified Control.Distributed.MPI.Store  as MPI
+import qualified Control.Monad.Catch as MC
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.STM (atomically)
+import Control.Monad.Trans.Class (lift)
 import qualified Control.Monad.STM as STM
 
 import Data.Store (Store)
@@ -25,17 +28,19 @@ import qualified Pipes.Prelude as P
 import qualified Pipes.Safe    as PS
 
 
-newtype JobInput e a = JobInput e
+newtype JobTagIn e a = JobTagIn e
   deriving (Eq, Ord, Show, Bounded, Enum)
 
-newtype JobResult e a = JobResult  e
+newtype JobTagOut e a = JobTagOut  e
   deriving (Eq, Ord, Show, Bounded, Enum)
 
+data JobTags e e' a b = JobTags (JobTagIn e a) (JobTagOut e' a)
 
-data StreamItem msg = StreamItem msg | StreamEnd
-  deriving (Eq, Ord, Generic)
 
-instance Store msg => Store (StreamItem msg)
+data StreamItem a = StreamItem a | StreamEnd
+  deriving (Eq, Ord, Show, Generic)
+
+instance Store a => Store (StreamItem a)
 
 
 messagesFrom :: forall tag m a. (Enum tag, PS.MonadSafe m, Store a)
@@ -43,35 +48,62 @@ messagesFrom :: forall tag m a. (Enum tag, PS.MonadSafe m, Store a)
              -> tag                           -- ^ tag
              -> MPI.Comm                      -- ^ communicator
              -> Producer a m ()               -- ^ produces values of producer tag
-messagesFrom rank tag comm = go
+messagesFrom rank tag comm = loop
   where
-    go :: Producer a m ()
-    go = do
-      msg <- liftIO $ MPI.recv_ rank (MPI.toTag tag) comm
+    tag' :: MPI.Tag
+    tag' = MPI.toTag tag
+
+    loop :: Producer a m ()
+    loop = do
+      msg <- liftIO $ MPI.recv_ rank tag' comm
 
       case msg of
-        StreamItem a -> P.yield a >> go
+        StreamItem a -> do
+          P.yield a
+          loop
         StreamEnd    -> return ()
 
 
+messagesTo :: forall tag m a. (Enum tag, PS.MonadSafe m, Store a)
+           => MPI.Rank
+           -> tag
+           -> MPI.Comm
+           -> Consumer a m ()
+messagesTo rank tag comm = do
+    P.mapM_ (\s -> liftIO $ MPI.send (StreamItem s) rank tag' comm)
+      `PS.finally` sendStreamEnd
+  where
+    tag' :: MPI.Tag
+    tag' = MPI.toTag tag
+
+    streamEnd :: StreamItem a
+    streamEnd = StreamEnd
+
+    sendStreamEnd :: PS.Base m ()
+    sendStreamEnd = liftIO $ MPI.send (StreamEnd @a) rank tag' comm
+
+
 makeWorker :: (PS.MonadSafe m, Enum tag, Enum tag', Store a, Store b)
-           => MPI.Rank -> JobInput tag a -> JobResult tag' b -> MPI.Comm -> (a -> m b) -> m ()
-makeWorker master jobTag resultTag comm f =
-  P.runEffect $
-    messagesFrom master jobTag comm
-      >-> P.mapM f
-      >-> P.mapM_ (\b -> liftIO $ MPI.send b master (MPI.toTag resultTag) comm)
+           => MPI.Rank
+           -> JobTags tag tag' a b
+           -> MPI.Comm
+           -> (a -> m b)
+           -> m ()
+makeWorker master (JobTags tagIn tagOut) comm f =
+    P.runEffect $
+        messagesFrom master tagIn comm
+          >-> P.mapM f
+          >-> messagesTo master tagOut comm
 
 
 delegate :: forall m a b tag tag' r. (PS.MonadSafe m, Enum tag, Enum tag', Store a, Store b)
          => [MPI.Rank]
-         -> JobInput tag a
-         -> JobResult tag' b
+         -> JobTags tag tag' a b
          -> MPI.Comm
          -> Natural
          -> Pipe a b m r
-delegate []    _       _       _    _         = error "delegate called with no workers"
-delegate ranks sendTag recvTag comm queueSize = do
+delegate []    _                      _    _         = error "delegate called with no workers"
+delegate ranks (JobTags tagIn tagOut) comm queueSize = do
   done <- liftIO $ TMVar.newEmptyTMVarIO
 
   jobs <- liftIO $ TBQ.newTBQueueIO queueSize
@@ -84,45 +116,55 @@ delegate ranks sendTag recvTag comm queueSize = do
     r <- insertJobs jobs results
 
     lastResults <- liftIO $ do
-      atomically $ do
-        TMVar.putTMVar done ()
-        mapM_ Async.waitSTM senders
-        TBQ.flushTBQueue results
+        atomically $ do
+          TMVar.putTMVar done ()
+          mapM_ Async.waitSTM senders
+          TBQ.flushTBQueue results
 
     P.each lastResults
 
     return r
   where
+    tagIn', tagOut' :: MPI.Tag
+    tagIn'  = MPI.toTag tagIn
+    tagOut' = MPI.toTag tagOut
+
     insertJobs :: TBQ.TBQueue a -> TBQ.TBQueue b -> Pipe a b m r
     insertJobs jobs results =
-      P.for P.cat $ \a -> do
-        newResults <- liftIO $ atomically $
-            STM.orElse (do TBQ.writeTBQueue jobs a
-                           return [])
-                       (do STM.check . not =<< TBQ.isEmptyTBQueue results
-                           TBQ.flushTBQueue results)
+        P.for P.cat $ \a -> do
+          newResults <- liftIO $ atomically $
+              STM.orElse (do TBQ.writeTBQueue jobs a
+                             return [])
+                         (do STM.check . not =<< TBQ.isEmptyTBQueue results
+                             TBQ.flushTBQueue results)
 
-        P.each newResults
+          P.each newResults
 
     startRankSend :: TBQ.TBQueue a
                   -> TBQ.TBQueue b
                   -> TMVar.TMVar ()
                   -> MPI.Rank
                   -> IO (Async.Async ())
-    startRankSend jobs results done rank = Async.async go
+    startRankSend jobs results done rank =
+        Async.async $
+            loop `MC.finally` MPI.send (StreamEnd @a) rank tagOut' comm
       where
-        go :: IO ()
-        go = do
+        loop :: IO ()
+        loop = do
           newJob <- atomically $
               STM.orElse (Just <$> TBQ.readTBQueue jobs)
                          (Nothing <$ TMVar.readTMVar done)
 
           case newJob of
             Just job -> do
-              r <- MPI.sendrecv_ (StreamItem job) rank (MPI.toTag sendTag) rank (MPI.toTag recvTag) comm
-              atomically $ TBQ.writeTBQueue results r
-              go
+              result <- MPI.sendrecv_ (StreamItem job) rank tagIn' rank tagOut' comm
+
+              case result of
+                StreamItem r -> do
+                  atomically $ TBQ.writeTBQueue results r
+                  loop
+
+                StreamEnd -> return ()
 
             Nothing -> do
-              MPI.send (StreamEnd @a) rank (MPI.toTag sendTag) comm
               return ()
